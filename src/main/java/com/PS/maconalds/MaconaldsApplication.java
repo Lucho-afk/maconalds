20d6209ef952e9bf4c0d97639d174d20e117aa14
Luciano Leone <lucianoleone2011@hotmail.es>
2022-05-15 13:09:58 -0300

    Inicio del tp de proyecto de software, aplicacion Mc Dondal's

---
+++ src/main/java/com/PS/maconalds/MaconaldsApplication.java
@@ -0,0 +1,13 @@
+package com.PS.maconalds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaconaldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaconaldsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PS/maconalds/MaconaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PS.maconalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaconaldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
